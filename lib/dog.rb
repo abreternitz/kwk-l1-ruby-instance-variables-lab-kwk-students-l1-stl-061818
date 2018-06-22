414e4b2ae79e9574cba7db57a43b77b9b7868201
abreternitz <aliceburian@gmail.com>
2018-06-22 18:00:28 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,16 @@
 
-class Dog
-  def initialize(dog_name)
-    @dog_name=dog_name
+class Do
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
   end
+ 
   def name
-  @dog_name
+    @this_dogs_name
   end
 end
-
-lassie = Dog.new ("Lassie")
-
+ 
+lassie = Dog.new
+lassie.name = "Lassie"
+ 
 puts lassie.name
